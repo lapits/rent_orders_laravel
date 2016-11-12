b8567d61b2d58890d6cdae39ed82dec8de1f0e4e
lapits <lapits@gmail.com>
2016-11-12 16:18:14 +0700

    создание базового шаблона главной страницы

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="ru-RU">
+<head>
+    <meta charset="UTF-8">
+    <title>Лист заказа</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    
+</head>
+<body>
+    
+    <div class="conteiner">
+        <h1 class="text-center">Лист заказа</h1>
+    </div>
+    
+    
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+</body>
+</html>
+
--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,10 @@
 | by your application. Just tell Laravel the URIs it should respond
 | to using a Closure or controller method. Build something great!
 |
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::any('/', ['uses' => 'HomeController@index', 'as' => 'home']);
